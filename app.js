21629aeb92c33a96dc25658da91b46a9be6cf765
SUN JING <sunjing1990123@126.com>
2017-02-26 19:24:07 +0800

    support assigning port

--- app.js
+++ app.js
@@ -1,5 +1,34 @@
 var http = require('http');
+var port = defaultPort = 8081;
 
-http.createServer(function(request, response){
-	response.end('hello world');
-}).listen(8081);
+
+
+var httpServer = {
+	init: init
+}
+
+function init(){
+	processArguments();
+	http.createServer(function(request,response){
+		response.end('hello world');
+	}).listen(port, function(error){
+		if(error){
+			console.error('Unable to listen on port',port,error);
+		}else{
+			console.log('Listening on port '+port);
+		}
+	});	
+}
+
+function processArguments() {
+	if(process.argv.length>2){
+		var argvs = process.argv.slice(2);
+		argvs.forEach(function(arg){
+			//get port
+			var portMatchResults = arg.match(/^port=(\d+)$/);
+			port = (portMatchResults&&portMatchResults[1])?portMatchResults[1]:defaultPort;			
+		});
+	}	
+}
+
+httpServer.init();
